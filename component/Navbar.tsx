150230a71d050aa014a34d31b0716047d5725d54
Ved Gupta <vedgupta@protonmail.com>
2022-02-18 10:54:11 +0530

    navbar Created

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -4,16 +4,21 @@ import Link from 'next/link';
 function Navbar() {
   return (
     <header>
-        <div>
-            <Link href="/">
-                <img className='w-44 object-contain cursor-pointer' src="https://ik.imagekit.io/nextinnovate/medium-sanity-app/medium-logo_DMvTEm10q.png" alt="Medium Logo" />
-            </Link>
-        </div>
-        <div>
+        <div className="">
+          <Link href="/">
+            <img className='w-44 object-contain cursor-pointer' src="https://ik.imagekit.io/nextinnovate/medium-sanity-app/medium-logo_DMvTEm10q.png" alt="Medium Logo" />
+          </Link>
 
+          <div className='hidden'>
+            Hello
+          </div>
+
+          <div>
+
+          </div>
         </div>
     </header>
   )
 }
 
-export default Navbar
+export default Navbar;
